6dbca6e28c99abff0a2f71b02233e4f2e4c52fef
Eka Putra <ekaputra@balitechy.com>
2013-04-09 11:19:49 +0800

    edit setup.py

--- setup.py
+++ setup.py
@@ -21,14 +21,14 @@ from setuptools import setup, find_packages
 
 setup(
     name = "PyOvpn",
-    version = "0.1.0",
-    url = 'https://bitbucket.org/ekaputra/pyovpn',
+    version = "0.2.0",
+    url = 'https://github.com/ekaputra07/pyovpn',
     description = 'Simple Python Open VPN Client.',
     license = 'GNU/GPL',
     author = 'Eka Putra',
     author_email = 'ekaputra@balitechy.com',
     packages = find_packages(),
-    install_requires = ['pexpect'],
+    install_requires = ['pexpect','pynotify'],
     data_files = [
                 ('/usr/share/pixmaps', ['data/pyovpn.svg']),
                 ('/usr/share/applications', ['data/pyovpn.desktop'])
